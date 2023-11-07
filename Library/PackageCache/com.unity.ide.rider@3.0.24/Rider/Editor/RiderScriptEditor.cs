a196b8b33c0bed3791046cdd768bfe335e4cd77b
Raymond Tsai <arethere21@gmail.com>
2023-11-07 03:28:42 -0500

    removed everything

--- Library/PackageCache/com.unity.ide.rider@3.0.24/Rider/Editor/RiderScriptEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6bd003874676087d27fd7300e99b36b6af94e8afd11e71ac38287cc5662b9778
-size 16314
--- Library/PackageCache/com.unity.visualscripting@1.9.0/Runtime/VisualScripting.Flow/ScriptMachine.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6e71fbb0d816a3a514fb9a7809aed403d0a9933cb848f2806ce640d9886b3378
-size 1835
