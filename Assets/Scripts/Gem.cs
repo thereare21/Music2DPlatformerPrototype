29e596db6981504d1d8025d201e18cc20b1d61e3
Raymond Tsai <arethere21@gmail.com>
2024-02-21 11:47:05 -0500

    added gitignore

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:844432e585affa28a4494b6ef85574788cfc76ac9945247c707afda71d789ccf
-size 765
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Class for allowing the Gem to be collected and add to the total score
+ */
+public class Gem : MonoBehaviour
+{
+    [SerializeField]
+    private int value = 1; //how much the gem adds to the score when collected
+    public static int totalscore = 0; //globally accessable variable for score
+  
+    /*
+     * Called when gem is collected (touched by player)
+     */
+    public void Collect()
+    {
+        IncrementScore(value);
+        Debug.Log("Collecting " + name + " Total score is now: " + totalscore);
+        Destroy(gameObject);
+    }
+
+    /*
+     * Adds amount to total score 
+     */
+    private void IncrementScore(int amount)
+    {
+        totalscore += amount;
+    }
+}
